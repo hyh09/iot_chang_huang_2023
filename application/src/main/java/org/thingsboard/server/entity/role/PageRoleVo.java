f5e4d2b7772acb03864eeaf3864f9bdea70801d8
wb04 <455663056@qq.com>
2021-10-25 14:30:00 +0800

    合并分支到dev  角色的增删改查

---
+++ application/src/main/java/org/thingsboard/server/entity/role/PageRoleVo.java
@@ -0,0 +1,13 @@
+package org.thingsboard.server.entity.role;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.ToString;
+import org.thingsboard.server.common.data.page.PageLink;
+
+
+@ToString
+@ApiModel("角色的分页实体类")
+public class PageRoleVo   {
+
+}
