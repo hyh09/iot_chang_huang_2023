1988f387e331bc692cab2fba5ddcaed63efa2c72
wb04 <455663056@qq.com>
2021-11-09 09:18:51 +0800

    ### 提交空实体类

---
+++ dao/src/main/java/org/thingsboard/server/dao/model/sqlts/timescale/ts/EffectTsKvEntity.java
@@ -0,0 +1,11 @@
+package org.thingsboard.server.dao.model.sqlts.timescale.ts;
+
+/**
+ * @program: thingsboard
+ * @description: 效能分析之产能分析的统计接口
+ * @author: HU.YUNHUI
+ * @create: 2021-11-09 09:18
+ **/
+
+public class EffectTsKvEntity {
+}
