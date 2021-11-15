e58698b4974b2abfa3f663bbe337a8372c67f6d8
wwj <wwj787373248@gmail.com>
2021-11-15 14:48:26 +0800

    修改查询设备字典列表接口

--- dao/src/main/java/org/thingsboard/server/dao/hs/dao/DictDeviceRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/hs/dao/DictDeviceRepository.java
@@ -1,5 +1,7 @@
 package org.thingsboard.server.dao.hs.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -19,7 +21,7 @@ import java.util.UUID;
 @Repository
 public interface DictDeviceRepository extends PagingAndSortingRepository<DictDeviceEntity, UUID>, JpaSpecificationExecutor<DictDeviceEntity> {
 
-    @Query("select new DictDeviceEntity(d.id, d.name) from DictDeviceEntity d where d.tenantId = :tenantId order by d.createdTime desc")
+    @Query("select d from DictDeviceEntity d where d.tenantId = :tenantId order by d.createdTime desc")
     List<DictDeviceEntity> findAllByTenantId(@Param("tenantId") UUID tenantId);
 
     Optional<DictDeviceEntity> findByTenantIdAndId(UUID tenantId, UUID id);
