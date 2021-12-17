edf23e5184f50b0e4e91f48640248fd837d94047
wb04 <455663056@qq.com>
2021-12-17 10:49:32 +0800

    能耗 产能的排除掉网关的设备#【禅道bug修复】

--- dao/src/main/java/org/thingsboard/server/dao/sql/role/dao/EffectTsKvRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/sql/role/dao/EffectTsKvRepository.java
@@ -106,6 +106,7 @@ public class EffectTsKvRepository {
         {
             sonSql01.append(" and  d1.factory_id = :factoryId");
             param.put("factoryId", queryTsKvVo.getFactoryId());
+            sonSql01.append("  and position('\"gateway\":true' in d1.additional_info)=0" );
         }
         if(queryTsKvVo.getWorkshopId() != null)
         {
@@ -199,6 +200,8 @@ public class EffectTsKvRepository {
         {
             sonSql01.append(" and  d1.factory_id = :factoryId");
             param.put("factoryId", queryTsKvVo.getFactoryId());
+            sonSql01.append("  and position('\"gateway\":true' in d1.additional_info)=0" );
+
         }
         if(queryTsKvVo.getWorkshopId() != null)
         {
