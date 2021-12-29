d04be2bc3e0ff869694b4065cfbe46182dff23ee
zhaosiming <zsm123123>
2021-12-29 14:13:37 +0800

    整合脚本

--- application/src/main/data/huansi/1.0.0/1.0.0.sql
+++ application/src/main/data/huansi/1.0.0/1.0.0.sql
@@ -797,3 +797,90 @@ ALTER TABLE IF EXISTS public.hs_system_version
 
 COMMENT ON TABLE public.hs_system_version
     IS '系统版本表';
+
+create table tb_energy_chart
+(
+    id                   uuid not null
+        constraint tb_energy_chart_pkey
+            primary key,
+    created_time         bigint,
+    created_user         uuid,
+    tenant_id            uuid,
+    updated_time         bigint,
+    updated_user         uuid,
+    capacity_added_value varchar(255),
+    capacity_first_time  bigint,
+    capacity_first_value varchar(255),
+    capacity_last_time   bigint,
+    capacity_value       varchar(255),
+    date                 date,
+    electric_added_value varchar(255),
+    electric_first_time  bigint,
+    electric_first_value varchar(255),
+    electric_last_time   bigint,
+    electric_value       varchar(255),
+    entity_id            uuid,
+    gas_added_value      varchar(255),
+    gas_first_time       bigint,
+    gas_first_value      varchar(255),
+    gas_last_time        bigint,
+    gas_value            varchar(255),
+    ts                   bigint,
+    water_added_value    varchar(255),
+    water_first_time     bigint,
+    water_first_value    varchar(255),
+    water_last_time      bigint,
+    water_value          varchar(255)
+);
+
+----新增表用于趋势图的统计时间差
+CREATE TABLE IF NOT EXISTS public.tb_enery_time_gap
+(
+    id uuid NOT NULL,
+    created_time bigint,
+    created_user uuid,
+    tenant_id uuid,
+    updated_time bigint,
+    updated_user uuid,
+    entity_id uuid,
+    key_name character varying(255) COLLATE pg_catalog."default",
+    time_gap bigint,
+    ts bigint,
+    value character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT tb_enery_time_gap_pkey PRIMARY KEY (id)
+)
+
+create table tb_statistical_data
+(
+    id                   uuid not null
+        constraint tb_statistical_data_pkey
+            primary key,
+    created_time         bigint,
+    created_user         uuid,
+    tenant_id            uuid,
+    updated_time         bigint,
+    updated_user         uuid,
+    capacity_added_value varchar(255),
+    capacity_first_time  bigint,
+    capacity_first_value varchar(255),
+    capacity_last_time   bigint,
+    capacity_value       varchar(255),
+    date                 date,
+    electric_added_value varchar(255),
+    electric_first_time  bigint,
+    electric_first_value varchar(255),
+    electric_last_time   bigint,
+    electric_value       varchar(255),
+    entity_id            uuid,
+    gas_added_value      varchar(255),
+    gas_first_time       bigint,
+    gas_first_value      varchar(255),
+    gas_last_time        bigint,
+    gas_value            varchar(255),
+    ts                   bigint,
+    water_added_value    varchar(255),
+    water_first_time     bigint,
+    water_first_value    varchar(255),
+    water_last_time      bigint,
+    water_value          varchar(255)
+);
