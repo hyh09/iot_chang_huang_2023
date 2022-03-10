35e767bd9af2d3810ef144ed2a0f569ae48f9a79
zhaosiming <zsm123123>
2022-03-10 19:27:07 +0800

    集团看板大屏生产监控

--- application/src/main/java/org/thingsboard/server/entity/productioncalender/vo/ProductionMonitorListVo.java
+++ application/src/main/java/org/thingsboard/server/entity/productioncalender/vo/ProductionMonitorListVo.java
@@ -5,11 +5,13 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.thingsboard.server.common.data.productioncalender.ProductionCalender;
 
-import java.util.UUID;
-
 @ApiModel(value = "ProductionMonitorListVo",description = "看板生产监控展示数据")
 @Data
 public class ProductionMonitorListVo {
+
+
+    @ApiModelProperty("工厂名称")
+    public String factoryName;
 
     @ApiModelProperty("设备名称")
     public String deviceName;
@@ -21,25 +23,16 @@ public class ProductionMonitorListVo {
     private String yearAchieve;
 
     @ApiModelProperty("生产状态")
-    private String productionState;
+    private Boolean productionState;
 
 
     public ProductionMonitorListVo(){}
 
     public ProductionMonitorListVo(ProductionCalender productionCalender) {
+        this.factoryName = productionCalender.getFactoryName();
         this.deviceName = productionCalender.getDeviceName();
         this.achieveOrPlan = productionCalender.getAchieveOrPlan();
         this.yearAchieve = productionCalender.getYearAchieve();
         this.productionState = productionCalender.getProductionState();
     }
-
-
-    @ApiModelProperty(value = "区间开始时间")
-    private Long startTime;
-    @ApiModelProperty(value = "区间结束时间")
-    private Long endTime;
-    @ApiModelProperty(value = "工厂Id")
-    private UUID factoryId;
-    @ApiModelProperty(value = "车间Id")
-    private UUID workshopId;
 }
