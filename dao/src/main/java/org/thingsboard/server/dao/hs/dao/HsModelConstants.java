3d5519f710fea5dde5e5d985163137eefa8e61fa
wwj <wwj787373248@gmail.com>
2022-03-10 15:57:47 +0800

    redunt

--- dao/src/main/java/org/thingsboard/server/dao/hs/dao/HsModelConstants.java
+++ dao/src/main/java/org/thingsboard/server/dao/hs/dao/HsModelConstants.java
@@ -168,6 +168,8 @@ public class HsModelConstants {
     public static final String ORDER_PLAN_MAINTAIN_START_TIME = "maintain_start_time";
     public static final String ORDER_PLAN_MAINTAIN_END_TIME = "maintain_end_time";
     public static final String ORDER_PLAN_FACTORY_ID = "factory_id";
+    public static final String ORDER_PLAN_WORKSHOP_ID = "workshop_id";
+    public static final String ORDER_PLAN_PRODUCTION_LINE_ID = "production_line_id";
 
     /**
      * 设备字典图表
