1bffef5d7431dc8acac468e5953b950508e73a0b
wb04 <455663056@qq.com>
2022-03-31 18:18:06 +0800

    Revert "【修复测试bug 2022-03-31】"
    This reverts commit 7ff6f199

--- dao/src/main/java/org/thingsboard/server/dao/util/StringUtilToll.java
+++ dao/src/main/java/org/thingsboard/server/dao/util/StringUtilToll.java
@@ -156,6 +156,11 @@ public class StringUtilToll {
     }
 
 
+    /**
+     * 是否是 0
+     * @param v2
+     * @return
+     */
     public  static  Boolean isZero(String v2)
     {
         BigDecimal b2 = new BigDecimal(v2);
@@ -164,6 +169,25 @@ public class StringUtilToll {
             return true;
         }
         return  false;
+
+    }
+
+
+
+
+    /**
+     * 是否是 0
+     * @param v2
+     * @return
+     */
+    public  static  Boolean isNotZero(String v2)
+    {
+        BigDecimal b2 = new BigDecimal(v2);
+        if(b2.compareTo(BigDecimal.ZERO)==0)
+        {
+            return false;
+        }
+        return  true;
 
     }
 
