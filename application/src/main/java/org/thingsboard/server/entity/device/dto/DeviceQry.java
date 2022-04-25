90c97ba43d020289cf4719584bf9dcd470af8856
zhaosiming <zsm123123>
2022-04-25 16:51:37 +0800

    自定义条件查询设备列表接口增加是否需要模型的过滤

--- application/src/main/java/org/thingsboard/server/entity/device/dto/DeviceQry.java
+++ application/src/main/java/org/thingsboard/server/entity/device/dto/DeviceQry.java
@@ -19,6 +19,9 @@ public class DeviceQry extends AbstractDevice{
     @ApiModelProperty("是否过滤设备图标")
     private Boolean filterIconFlag = false;
 
+    @ApiModelProperty("是否只查有设备模型的设备")
+    private Boolean hasModel = false;
+
     @Override
     public Device toDevice() {
         Device device = super.toDevice();
