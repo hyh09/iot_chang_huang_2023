d256459867e20158bcd4cb5c17d3c9becf1de86a
wb04 <455663056@qq.com>
2022-06-23 16:05:12 +0800

    ----

--- dao/src/main/java/org/thingsboard/server/dao/dingding/service/DingDingServer.java
+++ dao/src/main/java/org/thingsboard/server/dao/dingding/service/DingDingServer.java
@@ -58,44 +58,57 @@ public class DingDingServer implements DdingDingSendMssSvc {
     private DeviceDao deviceDao;
     @Autowired
     private EnergyLargeScreenReposutory energyLargeScreenReposutory;
-    @Autowired
-    private AttributesDao attributesDao;
-    @Autowired
-    private FactoryService factoryService;
+    @Autowired private  AttributesDao attributesDao;
+    @Autowired private FactoryService factoryService;
 
     private final RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
 
-    private String ACTIVE = "active";
+    private Map<UUID,String> mapFactory = new ConcurrentHashMap();
+
+    private  String ACTIVE="active";
+    private  String ADDITIONAL_INFO_FILED_NAME="gateway";
 
     @Override
     @Async("threadPoolTaskExecutor_1")
     public void send(UUID entityId, AttributeKvEntry entry) {
         try {
 
-            if (ACTIVE.equals(entry.getKey()) && entry.getBooleanValue() != null && !entry.getBooleanValue().get()) {
+
+            if(ACTIVE.equals(entry.getKey()) && entry.getBooleanValue() != null){
+//                if(entry.getBooleanValue().get()) {
+//                    return;
+//                }
                 Device device = deviceDao.findById(entityId);
                 if (device == null) {
                     log.error("钉钉发送查询不到设备,入参{}", entityId);
                     return;
                 }
+
                 UUID factoryId = device.getFactoryId();
                 if (factoryId == null) {
                     log.error("钉钉发送查询不到设备该工厂id,入参{}", entityId);
                     return;
                 }
                 Factory factory = factoryService.findById(factoryId);
+
+
                 List<UUID> deviceIdList = energyLargeScreenReposutory.getDeviceIdByVo(new EnergyHourVo(factoryId, null, null, null));
                 if (CollectionUtils.isEmpty(deviceIdList)) {
                     log.error("设备该工厂id查询不到设备了,入参{}", factoryId);
                     return;
                 }
-                Boolean flg = factoryIsOnline(deviceIdList, factoryId);
+                Boolean flg = factoryIsOnline(deviceIdList,factoryId);
+
                 if (!flg) {
                     toSendMess(new ParamVo(new ParamTextVo(factory.getName())));
                 }
             }
-        } catch (Exception e) {
-            log.error("【钉钉的异常信息]", e.getMessage());
+
+
+
+        }catch (Exception  e)
+        {
+            log.error("【钉钉的异常信息]",e.getMessage());
         }
 
     }
@@ -103,18 +116,24 @@ public class DingDingServer implements DdingDingSendMssSvc {
 
     /**
      * 判断该工厂是否在线
-     *
      * @param deviceIds
      * @return
      */
-    private Boolean factoryIsOnline(List<UUID> deviceIds, UUID factoryId) {
+    private Boolean factoryIsOnline( List<UUID> deviceIds, UUID factoryId ) {
         List<AttributeKvEntity> activeByDeviceIds = attributesDao.findActiveByDeviceIds(deviceIds);
         if (CollectionUtils.isNotEmpty(activeByDeviceIds)) {
             int online = (int) activeByDeviceIds.stream().filter(AttributeKvEntity::getBooleanValue).count();
-            if (online > 0) {
+            if(online>0)
+            {
+                mapFactory.put(factoryId,"不发");
                 return true;
             }
         }
+        if(StringUtils.isNotEmpty(mapFactory.get(factoryId))  && mapFactory.get(factoryId).equals("发"))
+        {
+            return true;
+        }
+        mapFactory.put(factoryId,"发");
         return false;
     }
 
@@ -156,4 +175,6 @@ public class DingDingServer implements DdingDingSendMssSvc {
     }
 
 
+
 }
+
