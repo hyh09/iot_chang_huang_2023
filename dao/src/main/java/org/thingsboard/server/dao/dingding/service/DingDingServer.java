8fa51ca72ff10b509cfa097f02c129d4752cd3fc
wb04 <455663056@qq.com>
2022-06-23 09:15:23 +0800

    ###提交长胜，避免重复发的问题

--- dao/src/main/java/org/thingsboard/server/dao/dingding/service/DingDingServer.java
+++ dao/src/main/java/org/thingsboard/server/dao/dingding/service/DingDingServer.java
@@ -64,6 +64,7 @@ public class DingDingServer implements DdingDingSendMssSvc {
     private FactoryService factoryService;
 
     private final RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+    private Map<UUID,String> mapFactory = new ConcurrentHashMap();
 
     private String ACTIVE = "active";
 
@@ -89,7 +90,7 @@ public class DingDingServer implements DdingDingSendMssSvc {
                     log.error("设备该工厂id查询不到设备了,入参{}", factoryId);
                     return;
                 }
-                Boolean flg = factoryIsOnline(deviceIdList);
+                Boolean flg = factoryIsOnline(deviceIdList,factoryId);
                 if (!flg) {
                     toSendMess(new ParamVo(new ParamTextVo(factory.getName())));
                 }
@@ -107,14 +108,21 @@ public class DingDingServer implements DdingDingSendMssSvc {
      * @param deviceIds
      * @return
      */
-    private Boolean factoryIsOnline(List<UUID> deviceIds) {
+    private Boolean factoryIsOnline(List<UUID> deviceIds,UUID factoryId ) {
         List<AttributeKvEntity> activeByDeviceIds = attributesDao.findActiveByDeviceIds(deviceIds);
         if (CollectionUtils.isNotEmpty(activeByDeviceIds)) {
             int online = (int) activeByDeviceIds.stream().filter(AttributeKvEntity::getBooleanValue).count();
             if (online > 0) {
+                mapFactory.put(factoryId,"不发");
                 return true;
             }
         }
+        if(StringUtils.isNotEmpty(mapFactory.get(factoryId))  && mapFactory.get(factoryId).equals("发"))
+        {
+            return true;
+        }
+        mapFactory.put(factoryId,"发");
+
         return false;
     }
 
