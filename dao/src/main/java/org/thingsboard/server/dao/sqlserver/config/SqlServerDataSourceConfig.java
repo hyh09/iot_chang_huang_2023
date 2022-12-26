d97507cc04d6387441773a38c91f66fd10b2b7f4
wb04 <455663056@qq.com>
2022-12-26 15:14:11 +0800

    pc端接口 提供外部接口--json的接口 待测试

--- dao/src/main/java/org/thingsboard/server/dao/sqlserver/config/SqlServerDataSourceConfig.java
+++ dao/src/main/java/org/thingsboard/server/dao/sqlserver/config/SqlServerDataSourceConfig.java
@@ -3,16 +3,27 @@ package org.thingsboard.server.dao.sqlserver.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @Project Name: thingsboard
@@ -24,10 +35,18 @@ import javax.sql.DataSource;
  */
 @Slf4j
 @Configuration
-public class SqlServerDataSourceConfig {
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "entityManagerFactoryMaster",//配置连接工厂 entityManagerFactory
+        transactionManagerRef = "transactionManagerMaster", //配置事物管理器  transactionManager
+        basePackages = {"org.thingsboard.server.dao.sqlserver.jpa.dao"} //配置主dao(repository)所在目录
+)public class SqlServerDataSourceConfig {
     @Autowired
     private Environment env;
-
+    @Autowired
+    private JpaProperties jpaProperties;
+    @Autowired
+    private HibernateProperties hibernateProperties;
 
 
     @Primary
@@ -65,6 +84,37 @@ public class SqlServerDataSourceConfig {
     public JdbcTemplate sqlServerTemplate(@Qualifier("sqlserverDataSource") DataSource dataSource2) {
         return new JdbcTemplate(dataSource2);
     }
+
+//
+//    @Bean("entityManagerSqlServer")
+//    public EntityManager entityManagerSqlServer(EntityManagerFactoryBuilder builder) {
+//        return Objects.requireNonNull(localContainerEntityManagerFactoryBean(builder).getObject()).createEntityManager();
+//    }
+//
+//    @Bean("entityManagerFactorySqlServer")
+//    public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean(EntityManagerFactoryBuilder builder) {
+//        return builder.dataSource(masterDataSource())
+//                .properties(getVendorProperties())
+//                //设置实体类所在目录
+//                .packages("org.thingsboard.server.dao.sqlserver.jpa.entity")
+//                //持久化单元名称，当存在多个EntityManagerFactory时，需要制定此名称
+//                .persistenceUnit("entityManagerFactorySqlServer")
+//                .build();
+//    }
+//
+//    private Map<String, Object> getVendorProperties() {
+//        return hibernateProperties.determineHibernateProperties(
+//                jpaProperties.getProperties(),
+//                new HibernateSettings()
+//        );
+//    }
+
+
+
+
+
+
+
 
 
 
