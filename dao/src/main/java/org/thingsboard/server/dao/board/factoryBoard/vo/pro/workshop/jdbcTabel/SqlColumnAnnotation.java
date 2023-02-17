52dafd452ca5eecf5fd63c6f9a2c4386c26d3747
wb04 <455663056@qq.com>
2023-02-17 14:12:05 +0800

    ####提交自定义注解

---
+++ dao/src/main/java/org/thingsboard/server/dao/board/factoryBoard/vo/pro/workshop/jdbcTabel/SqlColumnAnnotation.java
@@ -0,0 +1,23 @@
+package org.thingsboard.server.dao.board.factoryBoard.vo.pro.workshop.jdbcTabel;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Project Name: thingsboard
+ * @File Name: SqlColumnAnnotation
+ * @Date: 2023/2/17 14:08
+ * @author: wb04
+ * 业务中文描述:
+ * Copyright (c) 2023,All Rights Reserved.
+ */
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SqlColumnAnnotation {
+
+    String name() default "";
+
+    String queryWhere() default "";
+}
