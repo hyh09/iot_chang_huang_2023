bc52b54e2a92ab95d73e09332ead9ceebd2eb0b0
wb_04 <3510789527@qq.com>
2023-03-02 16:10:30 +0800

    提交空接口

--- application/src/main/java/org/thingsboard/server/controller/WorkshopEnergyController.java
+++ application/src/main/java/org/thingsboard/server/controller/WorkshopEnergyController.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.thingsboard.server.common.data.exception.ThingsboardErrorCode;
 import org.thingsboard.server.common.data.exception.ThingsboardException;
-import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.common.data.vo.QueryTsKvVo;
 import org.thingsboard.server.dao.board.factoryBoard.svc.FactoryEnergySvc;
-import org.thingsboard.server.dao.board.factoryBoard.vo.energy.current.CurrentUtilitiesVo;
-import org.thingsboard.server.dao.util.CommonUtils;
+import org.thingsboard.server.dao.board.workshopBoard.CapacitiesTop5Vo;
 import org.thingsboard.server.queue.util.TbCoreComponent;
+
+import java.util.List;
 
 /**
  * @Project Name: thingsboard
@@ -37,21 +36,9 @@ public class WorkshopEnergyController extends BaseController{
     private FactoryEnergySvc factoryEnergySvc;
 
 
-    @GetMapping("/queryCurrentEnergy11111")
+    @GetMapping("/queryCapacitiesTop5")
     @ResponseBody
-    public CurrentUtilitiesVo queryCurrentEnergy(QueryTsKvVo queryTsKvVo) throws ThingsboardException {
-        try {
-            TenantId tenantId = getTenantId();
-            if (queryTsKvVo.getStartTime() == null) {
-                queryTsKvVo.setStartTime(CommonUtils.getZero());
-                queryTsKvVo.setEndTime(CommonUtils.getNowTime());
-            }
-            CurrentUtilitiesVo currentUtilitiesVo = factoryEnergySvc.queryCurrentEnergy(queryTsKvVo, tenantId);
-            return currentUtilitiesVo;
-        } catch (Exception e) {
-            log.error("[工厂看板-能耗信息].queryCurrentEnergy方法异常入参:{}", queryTsKvVo);
-            log.error("[工厂看板-能耗信息].queryCurrentEnergy方法异常:{}", e);
-            throw new ThingsboardException(e.getMessage(), ThingsboardErrorCode.GENERAL);
-        }
+    public List<CapacitiesTop5Vo> queryCapacitiesTop5(QueryTsKvVo queryTsKvVo) throws ThingsboardException {
+        return  null;
     }
 }
