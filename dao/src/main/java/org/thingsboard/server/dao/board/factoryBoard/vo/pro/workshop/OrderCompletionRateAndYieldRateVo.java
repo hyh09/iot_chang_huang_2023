c6a90c45b499b2f83ae3fab237226beb673cc270
wb_04 <3510789527@qq.com>
2023-03-06 17:07:00 +0800

    ##bug 修复的，

--- dao/src/main/java/org/thingsboard/server/dao/board/factoryBoard/vo/pro/workshop/OrderCompletionRateAndYieldRateVo.java
+++ dao/src/main/java/org/thingsboard/server/dao/board/factoryBoard/vo/pro/workshop/OrderCompletionRateAndYieldRateVo.java
@@ -27,7 +27,7 @@ public class OrderCompletionRateAndYieldRateVo {
             "GROUP BY B1.usdOrderLotGUID\n" +
             ") D ON D.usdOrderLotGUID = C.uGUID"
             , postfixFlg = true,postTargetClass = FiledNamePostfixMethodUtil.class,postTargetMethod = "formatPercentage")
-    private String orderCompletionRate;
+    private String yieldRate;
 
     @SqlOnFieldAnnotation(value = "SELECT CAST((B.iCount-A.iCount)*100.0/B.iCount AS NUMERIC(18,2))\n" +
             "FROM (\n" +
@@ -42,7 +42,7 @@ public class OrderCompletionRateAndYieldRateVo {
             "JOIN (\n" +
             "SELECT iCount=COUNT(1) FROM dbo.sdOrderHdr A3(NOLOCK)\n" +
             ") B ON 1=1", postfixFlg = true,postTargetClass = FiledNamePostfixMethodUtil.class,postTargetMethod = "formatPercentage")
-    private String yieldRate;
+    private String  orderCompletionRate;
 
 
 }
