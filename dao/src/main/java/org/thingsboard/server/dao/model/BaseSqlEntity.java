bcc7bd0c2948c00b25e70908b254558588443b30
wb_04 <3510789527@qq.com>
2023-03-14 11:12:08 +0800

    Merge remote-tracking branch 'iot/test'
    # Conflicts:
    #	dao/src/main/java/org/thingsboard/server/dao/board/factoryBoard/vo/pro/workshop/jdbcTabel/query/SqlColumnAnnotation.java
    #	dao/src/main/java/org/thingsboard/server/dao/hs/service/Impl/MesServiceImpl.java

--- dao/src/main/java/org/thingsboard/server/dao/model/BaseSqlEntity.java
+++ dao/src/main/java/org/thingsboard/server/dao/model/BaseSqlEntity.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016-2021 The Thingsboard Authors
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +31,9 @@ import java.util.UUID;
 @MappedSuperclass
 public abstract class BaseSqlEntity<D> implements BaseEntity<D> {
 
+    /**
+     * 是有主键的
+     */
     @Id
     @Column(name = ModelConstants.ID_PROPERTY, columnDefinition = "uuid")
     @JpaOperatorsType(JpaQueryHelper.Operators.eq)
